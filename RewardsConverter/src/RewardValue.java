0cd15d8719a9acf67aec4595229d10615b30f632
Matthew Rivera <61847038+riveramatthew@users.noreply.github.com>
2024-08-10 19:54:56 -0400

    Add files via upload

---
+++ RewardsConverter/src/RewardValue.java
@@ -0,0 +1,29 @@
+package main.java;
+
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
+    // Constructor that accepts a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    // Constructor that accepts a value in miles
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    // Method to get the cash value of the RewardValue
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Method to get the miles value of the RewardValue
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
